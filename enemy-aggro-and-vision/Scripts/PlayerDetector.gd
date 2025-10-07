ec444e7dcba5618d7ce6fc3d60f8ecebb198e595
Roy Gero <roy.gero.ii@gmail.com>
2025-10-07 10:10:58 -0400

    Project #1 - Completed the concept.
    Areas for Refinement:
    - Right now the enemy is only looking for the player's center of mass. It'd be good to have a couple raycast points so the player can't "cheat" around corners.

--- enemy-aggro-and-vision/Scripts/PlayerDetector.gd
+++ enemy-aggro-and-vision/Scripts/PlayerDetector.gd
@@ -7,26 +7,44 @@ var meshMaterial: StandardMaterial3D
 @export var awareColor: Color
 @export var sleepingColor: Color
 
+var spaceState: PhysicsDirectSpaceState3D
+
 func _ready() -> void:
-	parentArea.body_entered.connect(ProcessBodyEnter)
-	parentArea.body_exited.connect(ProcessBodyExit)
-	
 	# Get the Material
 	meshMaterial = parentMesh.get_active_material(0) as StandardMaterial3D
 	assert(meshMaterial != null, "MeshInstance3D has no material assigned!")
-
-func ProcessBodyEnter(body):
-	if "Player" not in body.get_groups():
-		return;
-		
-	print("Player Entered? I am mad!")
-	if meshMaterial.albedo_color != awareColor:
-		meshMaterial.albedo_color = awareColor
 	
-func ProcessBodyExit(body):
-	if "Player" not in body.get_groups():
-		return;
-		
-	print("Player Exited! No longer Mad")
-	if meshMaterial.albedo_color == awareColor:
+	spaceState = get_world_3d().get_direct_space_state()
+	assert(spaceState != null, "Space State is fucked")
+	
+func _physics_process(delta: float) -> void:
+	var playerAround: bool = ProcessBodyTracking()
+	if playerAround && meshMaterial.albedo_color != awareColor:
+		meshMaterial.albedo_color = awareColor
+	elif !playerAround && meshMaterial.albedo_color == awareColor:
 		meshMaterial.albedo_color = sleepingColor
+		
+func ProcessBodyTracking() -> bool:
+	for body in parentArea.get_overlapping_bodies():
+		if "Player" not in body.get_groups():
+			continue
+		# Raycast to see if we can actually see the player.
+		var playerPos := (body as Node3D).global_transform.origin
+		
+		# This has changed in Godot 4.
+		var params = PhysicsRayQueryParameters3D.new()
+		params.from = global_transform.origin
+		params.to = playerPos
+		var raycastResult = spaceState.intersect_ray(params)
+		
+		# Check to see if it's the player.
+		if raycastResult:
+			var hitObject = raycastResult["collider"]
+			if !hitObject.is_in_group("Player"):
+				print("I can't see them")
+				continue
+		
+		return true
+		
+	# No bodies found
+	return false

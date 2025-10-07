224f76c155d696d79861264b92ecde73997b9997
Roy Gero <roy.gero.ii@gmail.com>
2025-10-07 09:18:11 -0400

    Project 1 - Making the enemy change color when they detect a player.

--- enemy-aggro-and-vision/Scripts/PlayerDetector.gd
+++ enemy-aggro-and-vision/Scripts/PlayerDetector.gd
@@ -1,12 +1,32 @@
 extends CollisionShape3D
 
-# Called when the node enters the scene tree for the first time.
+@onready var parentArea := $".."
+@onready var parentMesh := $"../../Mesh"
+
+var meshMaterial: StandardMaterial3D
+@export var awareColor: Color
+@export var sleepingColor: Color
+
 func _ready() -> void:
-	$"..".body_entered.connect(ProcessBodyEnter)
-	$"..".body_exited.connect(ProcessBodyExit)
+	parentArea.body_entered.connect(ProcessBodyEnter)
+	parentArea.body_exited.connect(ProcessBodyExit)
+	
+	# Get the Material
+	meshMaterial = parentMesh.get_active_material(0) as StandardMaterial3D
+	assert(meshMaterial != null, "MeshInstance3D has no material assigned!")
 
 func ProcessBodyEnter(body):
-	print("Body Entered? I am mad!")
+	if "Player" not in body.get_groups():
+		return;
+		
+	print("Player Entered? I am mad!")
+	if meshMaterial.albedo_color != awareColor:
+		meshMaterial.albedo_color = awareColor
 	
 func ProcessBodyExit(body):
-	print("Body Exited! No longer Mad")
+	if "Player" not in body.get_groups():
+		return;
+		
+	print("Player Exited! No longer Mad")
+	if meshMaterial.albedo_color == awareColor:
+		meshMaterial.albedo_color = sleepingColor
